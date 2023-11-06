46c698fa5a278c0d2d8c9832b30e19fcd60f093b
Clinton Nmereole <clintonnmereole@gmail.com>
2023-11-06 11:02:12 -0600

    trying to fix font on deploy 4, this time by adding @astrojs/vercel to project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
 import solidJs from "@astrojs/solid-js";
 import svelte from "@astrojs/svelte";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), solidJs(), svelte()]
+  integrations: [tailwind(), react(), solidJs(), svelte()],
+  output: "server",
+  adapter: vercel()
 });
