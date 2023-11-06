08406c01f91f567d16f90303a903d242cd80b95a
Clinton Nmereole <clintonnmereole@gmail.com>
2023-11-06 11:14:40 -0600

    removing unnecessary integrations from project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
-import solidJs from "@astrojs/solid-js";
-import svelte from "@astrojs/svelte";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), solidJs(), svelte()],
+  integrations: [tailwind()],
   output: "server",
   adapter: vercel(),
 });
